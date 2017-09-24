5df720f2e3eb157c59fd3f8e30d103b039c27d8e
Elizabeth <Elizabeth325325@gmail.com>
2017-09-23 22:30:47 -0500

    assignment finished!!!!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,31 +10,45 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
-    <body>
-      <!-- create your form here -->
-      cake  yummy!  bubbles are cool!  :)
+    <body> 
+        <form action="/" method="post">
+            <label>
+                Rotate by:
+                <input name="rot" type="text" value="0"></input>                
+            </label>
+            <textarea name="msg">{0}</textarea>
+            <button type="submit">submit</button>
+        </form
     </body>
 </html>
 """
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return form
+    return form.format("")
+
+@app.route("/", methods=["POST"])
+def encrypt():
+    print(request)
+    rot_amt = int(request.form['rot'])
+    stuff = request.form['msg']
+    x = rotate_string(stuff, rot_amt)
+    return form.format(x)
 
 app.run()
 
